ecca58dc318a948ebb288e88e4e29da901d1e49b
Manish Khadka <mrsomeonne@gmail.com>
2024-12-02 14:38:31 +0545

    DataType Conversion in JS

---
+++ JavaScript_Basics/datatype_conversion.js
@@ -0,0 +1,46 @@
+/* // Number(score);
+
+// let score = "33"; => 33
+// let score = "33abc"; => NaN
+// let score = null; => 0
+// let score; => NaN
+// let score = true; => 1
+// let score = false; => 0
+// let score = "manish"; => NaN
+let score = 33;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "33" (string) => 33 (number)
+// NaN => Not a Number
+// null => 0
+// undefined
+// true => 1, false => 0
+
+// let isLoggedIn = true; // true
+// let isLoggedIn; // false
+// let isLoggedIn = ""; //false
+// let isLoggedIn = null; //false
+let isLoggedIn ="manish"; // true
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "manish" => true
+// undefined => false
+
+ */
+
+let someNumber = 33; //=> "33"
+console.log(someNumber);
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// 33 => "33" (number to string)
