0d6079ef6aa32cecc5f93cf38c1be716ce7aa515
Manish Khadka <mrsomeonne@gmail.com>
2024-12-02 15:29:48 +0545

    Datatypes Summary JS

---
+++ JavaScript_Basics/datatypes_summary.js
@@ -0,0 +1,30 @@
+// PRIMITIVE DATATYPES
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+/* *************************************** */
+
+// REFERENCE / NON PRIMITIVE DATATYPES
+// array
+// object
+// function
+
+const num = [1, 2, 3, 4, 5];
+let myObj = {
+  name: "Manish",
+  age: 21
+}
+const myFunction = function(){
+  console.log("Hello, World");
+}
+
+// JavaScript is a Dynamic Typed Language which means the declaration of a variable is not required. And the data type is determined by the value assigned to it. Also can be changed during runtime.
+
+//In TypeScript you need to declare the datatypes.
+
+// While naming a variable make sure that the variable name is meaningful.
