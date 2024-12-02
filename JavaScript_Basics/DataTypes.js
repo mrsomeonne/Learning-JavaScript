951f7df669dbc0e69302e62ff1af86d4bee7896d
Manish Khadka <mrsomeonne@gmail.com>
2024-12-02 14:19:45 +0545

    DataTypes in JS

---
+++ JavaScript_Basics/DataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // Treat all JS code as a newer version
+console.log("Hello world!")
+// alert (3+3); // we are using nodejs and not browser , it shows error
+
+//DataTypes:
+let name ="Manish";
+let age=21;
+let isLoggedIn=false;
+
+//PRIMITIVE DATATYPES
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (empty_value) // not undefined
+// undefined => (value is not assigned)
+// symbol => unique (Mostly used in React)
+
+//OBJECT
+console.log(typeof "Manish"); // <- sting
+console.log(typeof undefined); // <- undefined
