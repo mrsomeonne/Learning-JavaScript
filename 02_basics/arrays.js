4a187da825416cde5bb8af5449f82380ec1836be
Manish Khadka <mrsomeonne@gmail.com>
2024-12-03 15:19:04 +0545

    Arrays pt1 in JS

---
+++ 02_basics/arrays.js
@@ -0,0 +1,65 @@
+/******************* ARRAY ********************* */
+
+// const array = [1, 2, 3, 4, 5];
+/* console.log(array); // [1, 2, 3, 4, 5]
+console.log(array[0]); // 1
+console.log(array.length); // 5
+
+let arr2 = new Array(1, 2, 3, 4, 5);
+console.log(arr2); // [1, 2, 3, 4, 5]
+console.log(arr2[0]); // 1
+console.log(arr2.length); // 5 */
+
+ // Array Methods
+/* // Push and Pop
+let myArray = [1, 2, 3, 4, 5];
+console.log(myArray); // [1, 2, 3, 4, 5] 
+
+myArray.push(6);
+console.log(myArray); // [1, 2, 3, 4, 5, 6]
+
+myArray.pop();
+console.log(myArray); */
+
+/* // Shift and Unshift
+let array1 = [1, 2, 3, 4, 5];
+console.log(array1); // [1, 2, 3, 4, 5]
+array1.unshift(9);
+console.log(array1); // [9, 1, 2, 3, 4, 5]
+
+array1.shift();
+console.log(array1); // [1, 2, 3, 4, 5] */
+
+/* // Includes and IndexOf
+let array3 = [1, 2, 3, 4, 5];
+console.log(array3.includes(9)); // false
+console.log(array3.indexOf(19)); // -1 //value not exists
+console.log(array3.indexOf(3)); // 2 // 3 is in index 2 */
+
+/* // JOIN
+const newArr = array.join();
+console.log(newArr); // 1,2,3,4,5 */
+
+/* // Slice and Splice
+
+console.log("A", array); // A [ 1, 2, 3, 4, 5 ]
+const newArr1 = array.slice(1,3);
+console.log(newArr1); // [ 2, 3 ]
+console.log("B", array); // B [ 1, 2, 3, 4, 5 ]
+
+const newArr2 = array.splice(1, 3);
+console.log("C", array); // C [ 1, 5 ]
+console.log(newArr2); // [ 2, 3, 4 ]
+
+// Difference between slice and splice is that slice returns a new array while splice mutates the original array */
+
+/******************** Arrays Pt.2 **************************/
+
+
+
+
+
+
+
+
+
