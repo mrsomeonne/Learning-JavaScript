d9a6d81b812a30fa5e55a6ac7f93028acbeff2cd
Manish Khadka <mrsomeonne@gmail.com>
2024-12-03 15:36:21 +0545

    Array Pt.2 in JS

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -55,11 +55,72 @@ console.log(newArr2); // [ 2, 3, 4 ]
 
 /******************** Arrays Pt.2 **************************/
 
+const marvel_heroes = ["Thor", "Iron Man", "Captain America"];
+const dc_heroes = ["Superman", "Batman", "Flash"];
+
+/* //PUSH
+marvel_heroes.push(dc_heroes); 
+console.log(marvel_heroes);
+// OUTPUT -> takes arrays as an element (here 3rd element is array itself)
+/* [
+  'Thor',
+  'Iron Man',
+  'Captain America',
+  [ 'Superman', 'Batman', 'Flash' ]
+] */ // */
+ 
+/* //CONCAT
+let allHeroes = marvel_heroes.concat(dc_heroes);
+console.log(allHeroes);
+//OUTPUT
+/* [
+  'Thor',
+  'Iron Man',
+  'Captain America',
+  'Superman',
+  'Batman',
+  'Flash'
+] */ //  */
+
+/* // SPREAD OPERATOR
+// Preferred more than concat
+const all_new_heroes = [...marvel_heroes, ...dc_heroes];
+console.log(all_new_heroes);
+// OUTPUT
+/* [
+  'Thor',
+  'Iron Man',
+  'Captain America',
+  'Superman',
+  'Batman',
+  'Flash'
+] */ // */
+ 
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]] 
+
+const real_another_array = another_array.flat(Infinity); // infinity depth (depth of array within a array like we have 3)
+
+console.log(real_another_array);
+// OUTPUT
+/* [
+  1, 2, 3, 4, 5,
+  6, 7, 6, 7, 4,
+  5
+] */
 
 
+ console.log(Array.isArray("Manish")); // false);
+ console.log(Array.isArray([1,2,3])); // true);
+ console.log(Array.from("Manish")); // ['M', 'a', 'n', 'i', 's', 'h']
+ console.log(Array.from({name:"Manish"})); // [] // returns empty array {INTERESTING}
+ 
+ let score1 = 100;
+ let score2 = 200;
+ let score3 = 300;
 
-
-
+ console.log(Array.of(score1, score2, score3)); // [100, 200, 300]
+ 
+  
 
 
 
