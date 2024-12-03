d70cb4e42f58749d0b27f06b81ee7868e5aead45
Manish Khadka <mrsomeonne@gmail.com>
2024-12-03 18:19:50 +0545

    Function Pt.2 in JS

--- 03_basics/function_and_parameter.js
+++ 03_basics/function_and_parameter.js
@@ -52,7 +52,7 @@ function sum(a, b) {  // a,b -> Parameters
 const result = sum(2,3);
 console.log("Result: ",result); // Result: 5 */
 
-
+/* // DEFAULT VALUE PARAMETERS
 function loginUserMessage(username = "sam") {  // saving sam as a default value
   if (username === undefined){                   // check if the argument is passed or not
     console.log("Please enter your username");
@@ -64,4 +64,50 @@ function loginUserMessage(username = "sam") {  // saving sam as a default value
 console.log(loginUserMessage("Manish")); // Manish Just Logged In!
 
 
-console.log(loginUserMessage()); // undefined Just Logged In!
+console.log(loginUserMessage()); // undefined Just Logged In! */
+
+/********************* FUNCTIONS Pt.2 *********************************/
+
+/* // If you don't know the number of Arguments that are passed (Like a cart in shopping)
+
+function calculateCartPrice (...num1){  // .... Rest or Spread operator
+  return num1;
+}
+// console.log(calculateCartPrice(21, 200, 211)); /// output still stays 21
+// now with ...num1 as a parameter
+
+console.log(calculateCartPrice(100, 200, 300,)); // output -> [ 100, 200, 300 ]
+
+
+function calculateCart (val1, val2, ...num1){  // .... Rest or Spread operator
+  return num1;
+}
+console.log(calculateCart(100, 200, 300, 400, 500)); // output -> [ 300, 400, 500 ]
+ */
+
+/* // Objects in Functions
+const user = {
+  username: "Manish",
+  price : 999
+}
+
+function handleObjects(anyObject) {
+  console.log(`Username is: ${anyObject.username} and Price is ${anyObject.price}`)
+}
+// handleObjects(user); */
+
+/* // Passing Direct Object
+handleObjects({
+  username: "Hitesh",
+  price: 200
+});
+
+// Passing Arrays
+const myNewArray = [100, 200, 300];
+function returnSecondValue(getArray){
+  return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray)); // 200
+
+console.log(returnSecondValue([10, 20, 30])); // 20 */
