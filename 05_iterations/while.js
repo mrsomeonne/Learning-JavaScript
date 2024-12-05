961c4b64ee50e7b8d28b6321023d2e94bccda1b3
Manish Khadka <mrsomeonne@gmail.com>
2024-12-05 14:58:34 +0545

    Do and DO_While Loop in JS

---
+++ 05_iterations/while.js
@@ -0,0 +1,33 @@
+// While Loops
+
+/* let index = 0;
+while (index <= 10){
+  console.log(`Value of index is :${index}`);
+  index = index + 2;
+}
+ */
+
+
+/* let myArray = ["Spider Man", "Iron Man", "Captain American"]
+let arr = 0;
+while(arr < myArray.length){
+  console.log(`Value is ${myArray[arr]}`);
+  arr++;
+} */
+
+
+// DO_While
+
+/* // syntax 
+do {
+  
+} while (condition); */
+
+// let score = 1;
+// let score = 11
+do {
+  console.log(`Value of score is: ${score}`);
+  score++;
+} while (score <= 10);
+
+// Will execute at least once even the condition is not full filled
