09322cf1ac253ff5d788693294359b08be20dca9
Manish Khadka <mrsomeonne@gmail.com>
2024-12-05 14:09:06 +0545

    Switch Case in JS

---
+++ 04_control_flow/stwich.js
@@ -0,0 +1,38 @@
+// SWITCH CASE
+
+/*//SYNTAX
+ switch (key) {
+  case value:
+    
+    break;
+
+  default:
+    break;
+} */
+
+const month = 4;
+
+switch (month) {
+  case 1:
+    console.log("Januar");
+    break;
+  case 2:
+    console.log("Februar");
+    break;
+  case 3:
+    console.log("März");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("Mai");
+    break;
+  case 6:
+    console.log("Juni");
+    break;
+
+  default:
+    console.log("Latter Half Of The Year");
+    break;
+}
